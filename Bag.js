b5a28324ecbe12b0d60b12aa179faf9dab90f6ac
MustafaAldelaimi <mustafa1007@yahoo.com>
2023-02-08 12:46:37 +0000

    Finish

--- Bag.js
+++ Bag.js
@@ -2,7 +2,20 @@ class Bag {
     // Include constructor Method with the following property: 
         // 1. Weight
             //1a. Should handle an exception in case user adds no weight and throw an error.
-  
+    constructor(weight) {
+        if (!weight) {
+            throw new Error("bag needs weight")
+        }
+        this.weight = weight
+     }
+    }
+
+try {
+let bag1 = new Bag()
+} catch (error) {
+console.log(error)
 }
 
 //Export the module
+
+module.exports = Bag
